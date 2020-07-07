42a3dd2312baa07b7a70a8b4a98b8f4f9920abbe
LorenaMendes <lorenamendes.contato@gmail.com>
2020-07-07 15:59:57 -0300

    Server list updated

--- discord-clone/src/components/ServerList/index.tsx
+++ discord-clone/src/components/ServerList/index.tsx
@@ -13,15 +13,15 @@ const ServerList: React.FC = () => {
       <Separator />
 
       <ServerButton />
+      <ServerButton hasNotifications mentions={3}/>
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
       <ServerButton hasNotifications />
-      <ServerButton mentions={3} />
       <ServerButton />
       <ServerButton />
-      <ServerButton />
-      <ServerButton hasNotifications />
-      <ServerButton />
-      <ServerButton />
-      <ServerButton mentions={72} />
+      <ServerButton hasNotifications mentions={72} />
       <ServerButton />
       <ServerButton />
     </Container>
